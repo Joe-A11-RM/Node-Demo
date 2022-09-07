c2ddab3a1655404c0b02d7de3f3229fdeb2a0877
Yousef Sherif <109547873+Joe-A11-RM@users.noreply.github.com>
2022-09-07 17:43:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var url = require('url')
 var fs = require('fs')
 
 http.createServer(function(request, response) {
-    var pathname = url.parse(request.url).pathname
+    //var pathname = url.parse(request.url).pathname
     console.log("Request For" + pathname + "is received")
-    fs.readFile(pathname.substr(1), function(err, data) {
+    fs.readFile('index.html', function(err, data) {
         if (err) {
             console.log(err)
             response.writeHead(404, { 'Content-Type': 'text/html' })
